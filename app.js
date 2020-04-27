912aec768070ef860e78aa5d354aa8c9d9ea1735
weissLL <weissll.267@gmail.com>
2020-04-27 15:05:31 +0900

    メモ化を利用し計算量を改善

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 'use strict';
 
-function fib(n) {
+// O(pow(2, n))
+
+/* function fib(n) {
 
     if (n === 0) return 0;
 
@@ -19,5 +21,33 @@ for (let i = 0; i <= length; i++) {
     let result = fib(i);
 
     console.log(result);
-    
+
+} */
+
+// O(n)
+
+const memo = new Map();
+
+memo.set(0, 0); memo.set(1, 1);
+
+function fib(n) {
+
+    if (memo.has(n)) return memo.get(n);
+
+    let value = fib(n - 1) + fib(n - 2);
+
+    memo.set(n, value);
+
+    return value;
+
+}
+
+let length = 40;
+
+for (let i = 0; i <= length; i++) {
+
+    let result = fib(i);
+
+    console.log(result);
+
 }
