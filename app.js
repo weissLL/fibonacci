8d6c5472c37c3f92d6ec773c8130cf031cfadf36
weissLL <weissll.267@gmail.com>
2020-04-27 14:43:53 +0900

    指数オーダーのフィボナッチ数列の実装

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function fib(n) {
+
+    if (n === 0) return 0;
+
+    if (n === 1) return 1;
+
+    // return null;
+
+    return fib(n - 1) + fib(n - 2);
+
+}
+
+let length = 40;
+
+for (let i = 0; i <= length; i++) {
+
+    let result = fib(i);
+
+    console.log(result);
+    
+}
